edf113a0bc5007c2e912a578a515e6c86d528a15
Selçuk Ermaya <selcukermaya@gmail.com>
2019-04-07 12:20:05 +0300

    Added .editorconfig.

--- compiled/modules/normal-filter.js
+++ compiled/modules/normal-filter.js
@@ -1,23 +1,29 @@
-'use strict';
+"use strict";
 
-var debounce = require('debounce');
+var debounce = require("debounce");
 
-module.exports = function (h) {
-    var _this = this;
+module.exports = function(h) {
+  var _this = this;
 
-    return function (classes, id) {
+  return function(classes, id) {
+    var search =
+      _this.source == "client"
+        ? _this.search.bind(_this, _this.data)
+        : _this.serverSearch.bind(_this);
 
-        var search = _this.source == 'client' ? _this.search.bind(_this, _this.data) : _this.serverSearch.bind(_this);
+    return h("input", {
+      class: classes.input + " " + classes.small,
+      attrs: {
+        type: "text",
+        placeholder: _this.display("filterPlaceholder"),
 
-        return h('input', { 'class': classes.input + ' ' + classes.small,
-            attrs: { type: 'text',
-                placeholder: _this.display('filterPlaceholder'),
-
-                id: id
-            },
-            on: {
-                'keyup': _this.opts.debounce ? debounce(search, _this.opts.debounce) : search
-            }
-        });
-    };
+        id: id
+      },
+      on: {
+        keyup: _this.opts.debounce
+          ? debounce(search, _this.opts.debounce)
+          : search
+      }
+    });
+  };
 };
